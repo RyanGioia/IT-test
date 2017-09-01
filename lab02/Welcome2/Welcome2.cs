e5f7ba18d9428bb692b39f555712cff2da2b11ab
RyanGioia <s01144244@acad.tri-c.edu>
2017-09-01 10:53:05 -0400

    Added lab02 files

---
+++ lab02/Welcome2/Welcome2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Welcome2
+{
+    class Welcome2
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
